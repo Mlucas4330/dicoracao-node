164f61aebccbe24d0a358ddb39da20b3fd179aa5
mlucas4330 <mlucas4330@gmail.com>
2025-02-11 19:26:44 -0300

    save

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
-import express from 'express'
+const express = require('express')
+const path = require('path')
 
 const app = express()
+
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'))
+
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+    res.render('index')
+})
 
 app.listen(3000, () => {
     'Servidor rodando na porta: ' + 3000
